5d8844f356bbc7e2df1b0270b9e5be153310ebd9
kartik <kartikvb92@gmail.com>
2026-04-16 15:14:55 +0530

    Final integration

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Student Management System")
+from add_student import add_student
+from view_students import view_students
+from delete_student import delete_student
+
+students = []
+
+while True:
+    print("\n1.Add 2.View 3.Delete 4.Exit")
+    ch = input("Enter choice: ")
+
+    if ch == "1":
+        add_student()
+    elif ch == "2":
+        view_students(students)
+    elif ch == "3":
+        students = delete_student(students)
+    elif ch == "4":
+        break
